a4696d5a97dc9d413a11947ab5946dc53712af6f
AleHashirama <alehashirama@gmail.com>
2024-02-07 17:15:47 +0100

    esercizio completo.

--- script.js
+++ script.js
@@ -1,20 +1,69 @@
+    const grid = document.querySelector("#grid-container");
 
-const grid = document.querySelector("#grid-container");
+    const button = document.getElementById("button");
 
-for(let i = 0; i < 100; i++) {
+    const select = document.getElementById("diff-select");
 
-    const myElement = document.createElement("div");
-    myElement.classList.add("square");
+    select.addEventListener("change", function() {
 
-    myElement.innerHTML = i + 1;
+        let difficult = select.value;
 
-    myElement.addEventListener("click", function() {
-        
-        console.log(this);
+        switch(difficult) {
 
-        this.classList.toggle("active");
+            case "easy":
+                console.log("Difficoltà selezionata: Easy");
+                break;
+            case "medium":
+                console.log("Difficoltà selezionata: Medium");
+                break;
+            case "expert":
+                console.log("Difficoltà selezionata: Expert");
+                break;
+            default:
+                console.log("Nessuna difficoltà selezionata");
+                break;
+        }
+    
+        button.addEventListener("click", function() {
+            grid.innerHTML = "";
+            if (difficult === "easy")
+
+            for (let i = 0; i < 100; i++) {
+    
+                const myElement = document.createElement("div");
+    
+                myElement.classList.add("square");
+    
+                myElement.innerHTML = i + 1;
+    
+                grid.append(myElement);
+    
+            } else if (difficult === "medium")
+
+            for (let i = 0; i < 81; i++) {
+    
+                const myElement = document.createElement("div");
+    
+                myElement.classList.add("square-medium");
+    
+                myElement.innerHTML = i + 1;
+    
+                grid.append(myElement);
+    
+            } else if (difficult === "expert")
+
+            for (let i = 0; i < 49; i++) {
+    
+                const myElement = document.createElement("div");
+    
+                myElement.classList.add("square-expert");
+    
+                myElement.innerHTML = i + 1;
+    
+                grid.append(myElement);
+    
+        };
     });
+});
 
-    grid.append(myElement);
-
-}
+    
